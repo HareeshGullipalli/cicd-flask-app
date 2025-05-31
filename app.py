5f993610b0c7d680f451b9fb29f42eda981f9780
Hareesh <hareeshgullipalli2698@gmail.com>
2025-05-31 16:43:10 +0530

    file modified

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Hareesh Gullipalli!"
+    return "Hello from Hareesh's 3rd container!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
